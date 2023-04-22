9f12aae934876734c010318c55670d35c2dacf61
NamAnhVH <namanh12022002@gmail.com>
2023-04-22 15:36:09 +0700

    hoàn thiện lại phần đăng ký, thêm role sử dụng

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module news.com/events
 
 go 1.20
 
-require github.com/go-sql-driver/mysql v1.7.0 // indirect
+require (
+	github.com/go-sql-driver/mysql v1.7.0 // indirect
+	github.com/gorilla/securecookie v1.1.1 // indirect
+	github.com/gorilla/sessions v1.2.1 // indirect
+)
--- src/SignUp/SignUp.go
+++ src/SignUp/SignUp.go
@@ -3,6 +3,7 @@ package SignUp
 import (
 	"database/sql"
 	"html/template"
+	"log"
 	"net/http"
 
 	_ "github.com/go-sql-driver/mysql"
@@ -13,53 +14,112 @@ type User struct {
 	Password string
 }
 
-func SignUpHandler() {
-	db, err := sql.Open("mysql", "root:namanh202@tcp(localhost:3306)/webhoctructuyen")
+type data struct {
+	Message string
+}
+
+var db *sql.DB
+var err error
+
+func openDB() {
+	db, err = sql.Open("mysql", "root:namanh202@tcp(localhost:3306)/webhoctructuyen")
 	if err != nil {
-		panic(err.Error())
+		log.Fatal(err)
 	}
-	defer db.Close()
+}
+
+func SignUpHandler() {
 
 	http.HandleFunc("/html/sign-up", func(w http.ResponseWriter, r *http.Request) {
+		label := data{
+			Message: "",
+		}
+
 		if r.Method == "GET" {
 			t, err := template.ParseFiles("html/sign-up/sign-up.html")
 			if err != nil {
 				http.Error(w, err.Error(), http.StatusInternalServerError)
 				return
 			}
-			t.Execute(w, nil)
+			err = t.Execute(w, label)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
 		} else if r.Method == "POST" {
 			username := r.FormValue("username")
 			password := r.FormValue("password")
 			hoten := r.FormValue("hoten")
+			role := r.FormValue("role")
 			// println(username)
 
-			_, err = db.Exec("INSERT INTO users (username, password, hoten) VALUES (?, ?, ?)", username, password, hoten)
-			if err != nil {
-				http.Error(w, err.Error(), http.StatusInternalServerError)
-				return
+			if checkUserExists(username) {
+				label := data{
+					Message: "Tài khoản đã tồn tại!",
+				}
+				// Hiện thông báo
+				t, err := template.ParseFiles("html/sign-up/sign-up.html")
+				if err != nil {
+					http.Error(w, err.Error(), http.StatusInternalServerError)
+					return
+				}
+				err = t.Execute(w, label)
+				if err != nil {
+					http.Error(w, err.Error(), http.StatusInternalServerError)
+					return
+				}
+			} else {
+				// adduser
+				addUser(username, password, hoten, role, w)
+
+				label := data{
+					Message: "Đăng ký thành công!",
+				}
+				//Hiện thông báo
+				tmpl, err := template.ParseFiles("html/sign-up/sign-up.html")
+				if err != nil {
+					http.Error(w, err.Error(), http.StatusInternalServerError)
+					return
+				}
+
+				err = tmpl.Execute(w, label)
+				if err != nil {
+					http.Error(w, err.Error(), http.StatusInternalServerError)
+					return
+				}
 			}
 
-			data := struct {
-				Message string
-			}{
-				Message: "Đăng ký thành công!",
-			}
-
-			tmpl, err := template.ParseFiles("html/sign-up/sign-up.html")
-			if err != nil {
-				http.Error(w, err.Error(), http.StatusInternalServerError)
-				return
-			}
-
-			err = tmpl.Execute(w, data)
-			if err != nil {
-				http.Error(w, err.Error(), http.StatusInternalServerError)
-				return
-			}
+			// http.Redirect(w, r, "/html/sign-up", http.StatusFound)
 
 		}
-	})
 
-	http.ListenAndServe(":8080", nil)
+	})
+}
+
+func checkUserExists(username string) bool {
+	openDB()
+
+	var count int
+	err = db.QueryRow("SELECT COUNT(*) FROM users WHERE username=?", username).Scan(&count)
+	if err != nil {
+		panic(err.Error())
+	}
+	db.Close()
+
+	if count > 0 {
+		return true
+	}
+	return false
+}
+
+func addUser(username string, password string, hoten string, role string, w http.ResponseWriter) {
+	openDB()
+
+	_, err = db.Exec("INSERT INTO users (username, password, hoten, role) VALUES (?, ?, ?, ?)", username, password, hoten, role)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	db.Close()
+
 }
